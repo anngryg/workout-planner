c654bc216ee54e27944094c7a38a5b506926d7bf
Anna Grygierczyk <anna.grygierczyk@wp.pl>
2023-11-28 22:28:28 +0100

    sidebar sketch

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import "../styles/Sidebar.scss";
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h1>Workout Planner</h1>
+      <button>+ Add New Plan</button>
+      <ul></ul>
+    </aside>
+  );
+}
